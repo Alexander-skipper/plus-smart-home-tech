c3802da969ef9ef5df87c78f7293020dc3bf3f2e
Alexander <alexanderskipper@yandex.ru>
2026-02-18 15:38:03 +0400

    fix collector

--- telemetry/collector/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.config;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
-import org.springframework.kafka.core.*;
 import ru.yandex.practicum.kafka.telemetry.event.*;
 import ru.yandex.practicum.serialization.HubEventAvroSerializer;
 import ru.yandex.practicum.serialization.SensorEventAvroSerializer;
@@ -14,25 +14,29 @@ import java.util.*;
 @Configuration
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    private Map<String, Object> props() {
-        Map<String, Object> map = new HashMap<>();
-        map.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        map.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return map;
+    private Properties getBaseProperties() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return props;
     }
 
     @Bean
-    public KafkaTemplate<String, SensorEventAvro> sensorTemplate() {
-        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(
-                props(), new StringSerializer(), new SensorEventAvroSerializer()));
+    public KafkaProducer<String, SensorEventAvro> sensorEventKafkaProducer() {
+        Properties props = getBaseProperties();
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                SensorEventAvroSerializer.class.getName());
+        return new KafkaProducer<>(props);
     }
 
     @Bean
-    public KafkaTemplate<String, HubEventAvro> hubTemplate() {
-        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(
-                props(), new StringSerializer(), new HubEventAvroSerializer()));
+    public KafkaProducer<String, HubEventAvro> hubEventKafkaProducer() {
+        Properties props = getBaseProperties();
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                HubEventAvroSerializer.class.getName());
+        return new KafkaProducer<>(props);
     }
 }
--- telemetry/collector/src/main/java/ru/yandex/practicum/service/KafkaEventProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/KafkaEventProducer.java
@@ -1,20 +1,23 @@
 package ru.yandex.practicum.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.kafka.telemetry.event.*;
 import ru.yandex.practicum.mapper.*;
 import ru.yandex.practicum.model.base.HubEvent;
 import ru.yandex.practicum.model.base.SensorEvent;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaEventProducer {
 
-    private final KafkaTemplate<String, SensorEventAvro> sensorTemplate;
-    private final KafkaTemplate<String, HubEventAvro> hubTemplate;
+    private final KafkaProducer<String, SensorEventAvro> sensorEventProducer;
+    private final KafkaProducer<String, HubEventAvro> hubEventProducer;
     private final SensorEventMapper sensorMapper;
     private final HubEventMapper hubMapper;
 
@@ -25,10 +28,42 @@ public class KafkaEventProducer {
     private String hubsTopic;
 
     public void sendSensorEvent(SensorEvent event) {
-        sensorTemplate.send(sensorsTopic, event.getId(), sensorMapper.toAvro(event));
+        try {
+            SensorEventAvro avroEvent = sensorMapper.toAvro(event);
+            ProducerRecord<String, SensorEventAvro> record =
+                    new ProducerRecord<>(sensorsTopic, event.getId(), avroEvent);
+
+            sensorEventProducer.send(record, (metadata, exception) -> {
+                if (exception != null) {
+                    log.error("Ошибка отправки события сенсора в Kafka", exception);
+                } else {
+                    log.debug("Событие сенсора отправлено: topic={}, partition={}, offset={}, key={}",
+                            metadata.topic(), metadata.partition(), metadata.offset(), event.getId());
+                }
+            });
+        } catch (Exception e) {
+            log.error("Ошибка при отправке события сенсора", e);
+            throw new RuntimeException("Ошибка отправки события сенсора в Kafka", e);
+        }
     }
 
     public void sendHubEvent(HubEvent event) {
-        hubTemplate.send(hubsTopic, event.getHubId(), hubMapper.toAvro(event));
+        try {
+            HubEventAvro avroEvent = hubMapper.toAvro(event);
+            ProducerRecord<String, HubEventAvro> record =
+                    new ProducerRecord<>(hubsTopic, event.getHubId(), avroEvent);
+
+            hubEventProducer.send(record, (metadata, exception) -> {
+                if (exception != null) {
+                    log.error("Ошибка отправки события хаба в Kafka", exception);
+                } else {
+                    log.debug("Событие хаба отправлено: topic={}, partition={}, offset={}, key={}",
+                            metadata.topic(), metadata.partition(), metadata.offset(), event.getHubId());
+                }
+            });
+        } catch (Exception e) {
+            log.error("Ошибка при отправке события хаба", e);
+            throw new RuntimeException("Ошибка отправки события хаба в Kafka", e);
+        }
     }
 }
